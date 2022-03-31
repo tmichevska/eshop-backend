15f62baa6b71e2e7557a0bf104405d442ca65dac
Robert Ilievski <rilievski54@gmail.com>
2022-03-31 21:54:54 +0200

    initial commit

---
+++ src/main/java/com/management/project/eshopbackend/EshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.management.project.eshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EshopBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/management/project/eshopbackend/EshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.project.eshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
