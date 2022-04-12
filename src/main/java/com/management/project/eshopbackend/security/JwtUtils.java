6566a2277137e15bd4ecd41b2f8d753186da498d
EmilijaT <67805805+EmilijaT@users.noreply.github.com>
2022-04-13 00:06:36 +0200

    Added validate JWT token method

--- src/main/java/com/management/project/eshopbackend/security/JwtUtils.java
+++ src/main/java/com/management/project/eshopbackend/security/JwtUtils.java
@@ -1,8 +1,7 @@
 package com.management.project.eshopbackend.security;
 
 import com.management.project.eshopbackend.service.impl.UserDetailsImpl;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -29,5 +28,24 @@ public class JwtUtils {
 
     public String getUserNameFromJwtToken(String token) {
         return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            System.out.printf("Invalid JWT signature: %s ", e.getMessage());
+        } catch (MalformedJwtException e) {
+            System.out.printf("Invalid JWT token: %s ", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            System.out.printf("JWT token is expired: %s ", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            System.out.printf("JWT token is unsupported: %s ", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.printf("JWT claims string is empty: %s ", e.getMessage());
+        }
+
+        return false;
     }
 }
