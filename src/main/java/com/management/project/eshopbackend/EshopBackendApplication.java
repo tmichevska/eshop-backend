6827e230f0abb002dc44d1bf71a5a14d80587ab2
bogdanovskaa <49886082+bogdanovskaa@users.noreply.github.com>
2022-04-16 01:34:22 +0200

    Anastasija -  JavaMailSender bean & ImageStroageService

--- src/main/java/com/management/project/eshopbackend/EshopBackendApplication.java
+++ src/main/java/com/management/project/eshopbackend/EshopBackendApplication.java
@@ -2,6 +2,11 @@ package com.management.project.eshopbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class EshopBackendApplication {
@@ -10,4 +15,22 @@ public class EshopBackendApplication {
     SpringApplication.run(EshopBackendApplication.class, args);
   }
 
+  @Bean
+  public JavaMailSender javaMailService() {
+    JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+    javaMailSender.setHost(this.host);
+    javaMailSender.setPort(this.port);
+    javaMailSender.setUsername(this.username);
+    javaMailSender.setPassword(this.password);
+
+    Properties javaMailProperties = new Properties();
+    javaMailProperties.put("mail.smtp.starttls.enable", "true");
+    javaMailProperties.put("mail.smtp.auth", "true");
+    javaMailProperties.put("mail.transport.protocol", "smtp");
+    javaMailProperties.put("mail.debug", "true");
+    javaMailProperties.put("mail.smtp.ssl.trust", "*");
+    javaMailSender.setJavaMailProperties(javaMailProperties);
+
+    return javaMailSender;
+  }
 }
