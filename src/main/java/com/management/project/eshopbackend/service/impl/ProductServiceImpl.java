fc1561e9f4f9d7d2fa4ae8cce23ce9cf9d205ea6
Dea <dea.pahumi@north-47.com>
2022-04-16 17:12:16 +0200

    Dea/ProductService
    Creating the ProductService model and interface

---
+++ src/main/java/com/management/project/eshopbackend/service/impl/ProductServiceImpl.java
@@ -0,0 +1,43 @@
+package com.management.project.eshopbackend.service.impl;
+
+import com.management.project.eshopbackend.models.exceptions.EntityNotFoundException;
+import com.management.project.eshopbackend.models.products.Product;
+import com.management.project.eshopbackend.repository.CategoryJPARepository;
+import com.management.project.eshopbackend.repository.ProductJPARepository;
+import com.management.project.eshopbackend.repository.AttributeJPARepository;
+import com.management.project.eshopbackend.service.intef.ShoppingCartService;
+import com.management.project.eshopbackend.service.intef.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductJPARepository productRepository;
+    private final CategoryJPARepository categoryRepository;
+    private final AttributeJPARepository attributeRepository;
+    private final ShoppingCartService shoppingCartService;
+
+
+    @Override
+    public Product findById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Product with id " + id + " not found!"));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> findAllByCategoryId(Long id) {
+        return productRepository.findAll().stream().filter(product -> product.getCategory().getId().equals(id)).collect(Collectors.toList());
+    }
+}
