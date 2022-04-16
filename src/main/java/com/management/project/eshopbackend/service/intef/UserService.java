7bb826a6a8fea1b69f9f89950dec69bb46cc0a32
EmilijaT <67805805+EmilijaT@users.noreply.github.com>
2022-04-16 15:03:20 +0200

    Created user service

---
+++ src/main/java/com/management/project/eshopbackend/service/intef/UserService.java
@@ -0,0 +1,24 @@
+package com.management.project.eshopbackend.service.intef;
+
+import com.management.project.eshopbackend.models.users.DTO.UserDTO;
+import com.management.project.eshopbackend.models.users.User;
+
+import javax.mail.MessagingException;
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getUsers();
+
+    User getUserById(Long userId);
+
+    User getUserByEmail(String email);
+
+    User createUser(UserDTO newUser) throws MessagingException;
+
+    User signUp(UserDTO newUser);
+
+    User updateUser(UserDTO userDTO);
+
+    User changeUserPassword (User User, String newPassword);
+}
