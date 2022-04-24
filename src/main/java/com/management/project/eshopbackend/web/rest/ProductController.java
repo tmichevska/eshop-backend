902d76c7de21a3560f900ae96b97b47b345f6d70
EmilijaT <67805805+EmilijaT@users.noreply.github.com>
2022-04-24 16:16:26 +0200

    Added CRUD methods in product controller

--- src/main/java/com/management/project/eshopbackend/web/rest/ProductController.java
+++ src/main/java/com/management/project/eshopbackend/web/rest/ProductController.java
@@ -20,21 +20,47 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getProduct(@PathVariable Long id){
+    public ResponseEntity<?> getProduct(@PathVariable Long id) {
         Product product;
         try {
             product = productService.findById(id);
-        }
-        catch (EntityNotFoundException ex){
+        } catch (EntityNotFoundException ex) {
             return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(Product.convertToDTO(product), HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<?> getAllProducts(){
+    public ResponseEntity<?> getAllProducts() {
         List<ProductDTO> products = productService.findAll().stream().map(Product::convertToDTO).collect(Collectors.toList());
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
+    @PostMapping("/create")
+    public ResponseEntity<?> createNewProduct(@RequestBody ProductDTO productDTO) {
+        Product product;
+        try {
+            product = productService.create(productDTO);
+        } catch (IllegalArgumentException ex) {
+            return new ResponseEntity<>(ex.toString(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(Product.convertToDTO(product), HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateProduct(@RequestBody ProductDTO productDTO) {
+        Product product;
+        try {
+            product = productService.update(productDTO);
+        } catch (IllegalArgumentException ex) {
+            return new ResponseEntity<>(ex.toString(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(Product.convertToDTO(product), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        productService.delete(id);
+        return new ResponseEntity<>("Product with id " + id + " deleted.", HttpStatus.OK);
+    }
 }
