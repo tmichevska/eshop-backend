4b93288c8fea14569d3d4dc9c3a3ed39e360efb1
Artan-Hasani <artanhasani999@gmail.com>
2022-04-28 11:44:29 +0200

    Methods getUser(), createUser() and updateUser() created in UserController

--- src/main/java/com/management/project/eshopbackend/web/rest/UserController.java
+++ src/main/java/com/management/project/eshopbackend/web/rest/UserController.java
@@ -1,16 +1,22 @@
 package com.management.project.eshopbackend.web.rest;
 
+import com.management.project.eshopbackend.models.users.DTO.UserDTO;
+import com.management.project.eshopbackend.models.users.User;
 import com.management.project.eshopbackend.service.intef.AuthTokenService;
 import com.management.project.eshopbackend.service.intef.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
 
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/api/users")
 public class UserController {
 
@@ -23,5 +29,28 @@ public class UserController {
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
     }
 
+    @GetMapping(value = "/{userId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> getUser(@PathVariable("userId") Long userId) {
+        return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> createUser(@RequestBody @Validated UserDTO userDTO) throws MessagingException {
+        User user = userService.createUser(userDTO);
+        String city = userDTO.getCity();
+        if (city != null) {
+            userService.createPostman(user, city);
+        }
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> updateUser(@RequestBody @Validated UserDTO userDTO) {
+        User user = userService.updateUser(userDTO);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 
 }
